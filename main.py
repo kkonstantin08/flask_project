2d33789ad9f06daf87ffd7703a5b94758b542a72
kkonstantin <kutasovkosta90@gmail.com>
2024-03-19 20:48:55 +0300

    +

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import Length, DataRequired
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SECRET_KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+
+db = SQLAlchemy(app)
+
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    created_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 news_set = [{'title': 'Удивительное событие в школе',
              'text': 'Вчера в местной школе произошло удивительное событие - все '
                      'ученики одновременно зевнули на уроке математики. '
@@ -42,28 +56,30 @@ class NewsForm(FlaskForm):
 
 @app.route('/')
 def index():
+    news_list = News.query.all()
     return render_template('index.html',
-                           news=news_set)
+                           news=news_list)
 
 
 @app.route('/news_detail/<int:id>')
 def news_detail(id):
-    title = news_set[id]['title']
-    text = news_set[id]['text']
+    news_detail = News.query.get(id)
     return render_template('news_detail.html',
-                           title=title,
-                           text=text)
+                           news=news_detail)
 
 
 @app.route('/add_news', methods=['GET', 'POST'])
 def add_news():
     form = NewsForm()
     if form.validate_on_submit():
-        title = form.title.data
-        text = form.text.data
-        news_set.append({'title': title, 'text': text})
-        return redirect(url_for('index'))
-    return render_template('add_news.html', form=form)
+        news = News()
+        news.title = form.title.data
+        news.text = form.text.data
+        db.session.add(news)
+        db.session.commit()
+        return redirect(url_for('news_detail', id=news.id))
+    return render_template('add_news.html',
+                           form=form)
 
 
 # @app.route('/news_set')
